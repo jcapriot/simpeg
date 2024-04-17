7835860ddc0418c4455d8ea6b1b84f64f9eb905e
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-04-17 01:19:01 -0600

    add more info

--- .github/workflows/scripts/reviewdog_flake8.sh
+++ .github/workflows/scripts/reviewdog_flake8.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 set -eu # Increase bash strictness
+set -o pipefail
 
 export REVIEWDOG_VERSION=v0.15.0
+
+echo ${CI_PULL_REQUEST}
+echo ${CI_COMMIT}
+echo ${CI_REPO_OWNER}
+echo ${CI_REPO_NAME}
+echo ${CI_REPOSITORY}
+echo ${CI_BRANCH}
 
 echo "Installing reviewdog..."
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
 
-echo "Flake8 version:"
-flake8 --version
+echo "Flake8 version: $(flake8 --version)"
 
 echo "Checking python code with the flake8 linter and reviewdog..."
-exit_val="0"
 
-flake8 ${WORKDIR} --config=.flake8 2>&1 |
-  /tmp/reviewdog -f=flake8 \
+flake_exit_val="0"
+flake8_output="$(flake8 ${WORKDIR} --config=.flake8 2>&1)" || flake_exit_val="$?"
+
+echo "flake8 output:"
+echo "${flake8_output}"
+
+exit_val="0"
+echo "${flake8_output}" | /tmp/reviewdog -f="flake8" \
     -name="flake8" \
     -reporter="github-pr-review" \
     -filter-mode="added" \
